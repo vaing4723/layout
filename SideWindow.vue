b22b4031a54f97a418989edcbf2c02bee3f116ae
vaing4723 <380953964@qq.com>
2024-09-04 01:21:19 +0800

    Add files via upload

---
+++ SideWindow.vue
@@ -0,0 +1,23 @@
+<!-- components/SideWindow.vue -->
+<template>
+    <div class="side-window">
+      <h3>侧边窗口</h3>
+      <div class="content">{{ window.content }}</div>
+    </div>
+  </template>
+  
+  <script setup>
+  defineProps(['window'])
+  </script>
+  
+  <style scoped>
+  .side-window {
+    border: 1px solid #000;
+    padding: 5px;
+    box-sizing: border-box;
+  }
+  
+  .content {
+    font-size: 14px;
+  }
+  </style>
